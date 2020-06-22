442f58aec992336f54ad68295ccf8f2dcba125f3
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-06-22 15:47:28 +0300

    EM-180

--- modules/EmergencyMedicine/sql/install.sql
+++ modules/EmergencyMedicine/sql/install.sql
@@ -234,7 +234,6 @@ VALUES ('reason_codes_1', 'clinikal_reason_codes', 'Filter', '1');
 
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`)
 VALUES
-('lists', 'clinikal_app_secondary_statuses', 'Clinikal Appointment Secondary Statuses', 0, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
 ('clinikal_app_secondary_statuses', 'waiting_for_nurse', 'Waiting for Nurse', 10, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
 ('clinikal_app_secondary_statuses', 'waiting_for_doctor', 'Waiting for Doctor', 20, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
 ('clinikal_app_secondary_statuses', 'waiting_for_xray', 'Waiting for X-ray', 30, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
