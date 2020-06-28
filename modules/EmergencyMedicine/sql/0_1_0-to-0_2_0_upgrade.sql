6679ec6b52ccbb93f68c9b3569e58a4b9c3c457d
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-06-28 15:43:30 +0300

    bug fix

--- modules/EmergencyMedicine/sql/0_1_0-to-0_2_0_upgrade.sql
+++ modules/EmergencyMedicine/sql/0_1_0-to-0_2_0_upgrade.sql
@@ -288,11 +288,17 @@ VALUES ('reason_codes_1', 'clinikal_reason_codes', 'Filter', '1');
 #EndIf
 
 
-#IfNotRow2D list_options list_id clinikal_app_secondary_statuses option_id waiting_for_nurse
+#IfNotRow2D list_options list_id clinikal_enc_secondary_statuses option_id waiting_for_nurse
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`)
 VALUES
-('clinikal_app_secondary_statuses', 'waiting_for_nurse', 'Waiting for Nurse', 10, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
-('clinikal_app_secondary_statuses', 'waiting_for_doctor', 'Waiting for Doctor', 20, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
-('clinikal_app_secondary_statuses', 'waiting_for_xray', 'Waiting for X-ray', 30, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
-('clinikal_app_secondary_statuses', 'waiting_for_release', 'Waiting for Release', 40, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1);
+('clinikal_enc_secondary_statuses', 'waiting_for_nurse', 'Waiting for Nurse', 10, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
+('clinikal_enc_secondary_statuses', 'waiting_for_doctor', 'Waiting for Doctor', 20, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
+('clinikal_enc_secondary_statuses', 'waiting_for_xray', 'Waiting for X-ray', 30, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1),
+('clinikal_enc_secondary_statuses', 'waiting_for_release', 'Waiting for Release', 40, 0, 0, '', 'In Progress ', '', 0, 0, 1, '', 1);
+#EndIf
+
+#IfRow list_options list_id clinikal_app_secondary_statuses
+
+DELETE FROM list_options WHERE list_id = "clinikal_app_secondary_statuses" OR option_id ="clinikal_app_secondary_statuses";
+
 #EndIf
